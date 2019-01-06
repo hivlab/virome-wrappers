8682480b0de2e0cfb41de7c765a1d42b27c84b3b
Taavi Päll <tapa741@gmail.com>
2019-01-06 18:38:07 +0200

    frac str to float

--- seqtk/wrapper.py
+++ seqtk/wrapper.py
@@ -7,12 +7,11 @@ from snakemake.shell import shell
 
 # Get parameters
 seed = snakemake.params.get("seed", "11")
-frac = snakemake.params.get("frac", "1")
+frac = float(snakemake.params.get("frac", "1"))
 
 # Command to run
 if (frac > 0 and frac < 1):
     cmd = "seqtk sample -s{seed} {snakemake.input[%s]} {frac} > {snakemake.output[%s]}"
-
 else:
     cmd = "ln -sr {snakemake.input[%s]} {snakemake.output[%s]}"
 
