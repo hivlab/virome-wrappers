621ca1eb3b25b684d4e8e645a57c5d900940d5c1
Taavi Päll <tapa741@gmail.com>
2019-01-29 19:01:14 +0200

    added bbwrap wrapper

---
+++ bbmap/bbwrap/wrapper.py
@@ -0,0 +1,24 @@
+__author__ = "Taavi Päll"
+__copyright__ = "Copyright 2019, Taavi Päll"
+__email__ = "tapa741@gmail.com"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+
+# Check inputs/arguments.
+def arg_c(args):
+   """"Concatenates input/output arguments with names.""""
+   argdict = dict(args)
+   argdict.update((k, k + "=" + v) for k,v in argdict.items())
+   return " ".join(list(argdict.values()))
+
+# Get input/output and optional flags.
+inputs = arg_c(snakemake.input)
+outputs = arg_c(snakemake.output)
+options = snakemake.params.get("options", "")
+
+# Run commands.
+shell("bbwrap.sh"
+      " {inputs}"
+      " {outputs}"
+      " {options}")
