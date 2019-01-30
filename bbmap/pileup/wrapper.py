95c5ced1737f1b88b08d29a364a7eda72364b250
Taavi Päll <tapa741@gmail.com>
2019-01-30 15:31:22 +0200

    test

--- bbmap/pileup/wrapper.py
+++ bbmap/pileup/wrapper.py
@@ -18,7 +18,13 @@ if len(snakemake.input) == 1:
 else:
    inputs = arg_c(snakemake.input)
 
+
+print("snakemake.input is " + snakemake.input)
+print("Length of snakemake.input is " + len(snakemake.input))
+print("Print out inputs:")
 print(inputs)
+
+print("Asserting...")
 assert len(inputs) > 0, "Input error. Input can have only one unnamed value assigned to variable 'in'. All other inputs must be named."
 outputs = arg_c(snakemake.output)
 options = snakemake.params.get("options", "")
