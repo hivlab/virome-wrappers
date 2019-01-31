4ca07ee171eb2cf0aacd61843e9ce2e89e713a41
Taavi Päll <tapa741@gmail.com>
2019-01-31 10:09:56 +0200

    bugfix

--- bbmap/bbwrap/wrapper.py
+++ bbmap/bbwrap/wrapper.py
@@ -15,7 +15,7 @@ def arg_c(args):
 # Get input/output and optional flags.
 # First see if we have unnamed values to 'in' argument.
 args = dict(snakemake.input)
-unnamed = [v for v in input if v not in set(args.values())]
+unnamed = [v for v in snakemake.input if v not in set(args.values())]
 
 # Get named arguments.
 inputs = arg_c(snakemake.input)
