f8d110da52b78ec8cb1a38af6111eb447aac53c4
Taavi Päll <tapa741@gmail.com>
2019-05-28 12:29:55 +0300

    check if file paths are strings

--- assembly/filter_coverage/wrapper.py
+++ assembly/filter_coverage/wrapper.py
@@ -6,11 +6,17 @@ __license__ = "MIT"
 import pandas as pd
 from Bio import SeqIO
 
+if not isinstance(snakemake.input.coverage, str):
+    raise ValueError("coverage must be file path")
+
+if not isinstance(snakemake.input.contigs, str):
+    raise ValueError("contigs must be file path")
+
 # Filter contigs table by coverage
 coverage = snakemake.params.get("avg_coverage", 2)
 
 # Import coverage table
-cov = pd.read_table(snakemake.input.coverage)
+cov = pd.read_csv(snakemake.input.coverage, sep = "\t")
 good_coverage = cov["Avg_fold"] >= float(coverage)
 cov_filtered = cov[good_coverage]
 ids = list(cov_filtered["#ID"])
