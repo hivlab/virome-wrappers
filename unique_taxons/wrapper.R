fb2753f1a6dc1613cc78d8ca53967105ea4d0538
Taavi Päll <tapa741@gmail.com>
2019-05-30 18:10:28 +0300

    unlist input and fix missing object and added comments

--- unique_taxons/wrapper.R
+++ unique_taxons/wrapper.R
@@ -2,7 +2,7 @@
 
 stopifnot(length(snakemake@output) == 1)
 
-#' Loading libraries
+message("Loading libraries.")
 library(dplyr)
 library(readr)
 library(purrr)
@@ -10,52 +10,52 @@ library(tidyr)
 
 parse_taxonomy <- function(input, output) {
 
-  all_files <- tibble(file = input)
+  message("Importing Blast results\n")
+  all_files <- tibble(file = unlist(input))
   queries <- all_files %>%
     mutate(data = map(file, read_csv, col_types = "cccdiiiiiiiddccccccccccccc")) %>%
     unnest()
 
-  #' Hits per query
-  #' Queries can have multple blast hits, we need to identify most plausble hits for each query.
-  #' 1) By evalue,
-  #' 2) Ranking by pident has second best impact.
-  #' Also dropping gi column to select rows with unique query tax_id combinations.
+  message("Hits per query\n
+  Queries can have multple blast hits, we need to identify most plausble hits for each query.\n
+  1) By evalue,\n
+  2) Ranking by pident has second best impact.\n
+  Also dropping gi column to select rows with unique query tax_id combinations.\n")
   top_queries <- queries %>%
     group_by(query) %>%
     filter(dplyr::min_rank(evalue) == 1, dplyr::min_rank(desc(pident)) == 1)
   
-  #' Select rows with unique query tax_id combinations.
+  message("Selecting rows with unique query tax_id combinations.\n")
   unique_top_queries <- top_queries %>%
     select(query, tax_id, parent_tax_id) %>% 
     ungroup %>%
     distinct()
   
-  #' Filter by taxon abundance
-  #' Calculate weighted taxon counts for group
-  #+ tax-per-query
+  message("Filter by taxon abundance\n
+  Calculating weighted taxon counts for group tax-per-query")
   abun_top_queries <- unique_top_queries %>%
     add_count(tax_id) %>%
     group_by(query) %>%
     top_n(1, n)
   
-  #' Summarise number of taxons by query.
+  message("Summarising number of taxons by query.\n")
   per_query <- abun_top_queries %>%
     select(query, tax_id) %>%
     distinct() %>%
     count(query)
   
-  #' List of queries with multiple tax_ids but unique parent_taxi_id
+  message("List of queries with multiple tax_ids but unique parent_taxi_id.\n")
   distinct_parent_taxid <- per_query %>%
     filter(n > 1) %>%
-    left_join(unamb_top_queries) %>%
+    left_join(abun_top_queries) %>%
     select(query, parent_tax_id) %>%
     distinct() %>%
     count(query) %>%
     filter(n == 1) %>%
     pull(query)
   
-  #' Assigning parent_tax_id to tax_id for queries with multiple tax_ids but unique parent_taxi_id
-  #' Dropping ambiguous queries
+  message("Assigning parent_tax_id to tax_id for queries with multiple tax_ids but unique parent_taxi_id\n
+  Dropping ambiguous queries.\n")
   unique_queries <- abun_top_queries %>%
     ungroup() %>%
     mutate(tax_id = case_when(
