5de2cf4ec3c17f0f8d5b95fcd2764e476031340f
Taavi Päll <tapa741@gmail.com>
2019-06-14 17:33:22 +0300

    update input wrangling

--- assembly/subset/wrapper.py
+++ assembly/subset/wrapper.py
@@ -6,7 +6,7 @@ def subset_records(source, ids):
         if record.id.split()[0] in ids:
             yield record
     
-virids = pd.read_csv(snakemake.input.virids).set_index("query", drop = False).index.to_list()
-subset = subset_records(SeqIO.parse(snakemake.input.contigs, "fasta"), virids)
+virids = pd.read_csv(snakemake.input.virids[0]).set_index("query", drop = False).index.to_list()
+subset = subset_records(SeqIO.parse(snakemake.input.contigs[0], "fasta")x, virids)
 with open(snakemake.output, "w") as out:
     SeqIO.write(subset, out, 'fasta')
