1ed9023548b04a55e8bf157882c795a067df9b28
Taavi Päll <tapa741@gmail.com>
2019-06-14 16:04:58 +0300

    added assembly subset wrapper

---
+++ assembly/subset/wrapper.py
@@ -0,0 +1,12 @@
+from Bio import SeqIO
+import pandas as pd
+    
+def subset_records(source, ids):
+    for record in source:
+        if record.id.split()[0] in ids:
+            yield record
+    
+virids = pd.read_csv(snakemake.input.virids).set_index("query", drop = False).index.to_list()
+subset = subset_records(SeqIO.parse(snakemake.input.contigs, "fasta"), virids)
+with open(snakemake.output, "w") as out:
+    SeqIO.write(subset, out, 'fasta')
