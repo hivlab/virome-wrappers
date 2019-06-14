69434f4bf1b705accff681f5748f5d0432f0da59
Taavi Päll <tapa741@gmail.com>
2019-06-14 17:59:54 +0300

    function update

--- assembly/subset/wrapper.py
+++ assembly/subset/wrapper.py
@@ -1,12 +1,11 @@
 from Bio import SeqIO
 import pandas as pd
     
-def subset_records(source, ids):
-    for record in source:
-        if record.id.split()[0] in ids:
-            yield record
-    
-virids = pd.read_csv(snakemake.input.virids[0]).set_index("query", drop = False).index.to_list()
-subset = subset_records(SeqIO.parse(snakemake.input.contigs[0], "fasta"), virids)
-with open(snakemake.output, "w") as out:
-    SeqIO.write(subset, out, 'fasta')
+def subset_records(virids, contigs, output):
+    ids = pd.read_csv(virids).set_index("query", drop = False).index.to_list()
+    with open(output, "w") as output_handle:
+        for record in SeqIO.parse(contigs, "fasta"):
+            if record.id.split()[0] in ids:
+                SeqIO.write(record, output_handle, 'fasta')
+
+subset_records(snakemake.input**, snakemake.output**)
