0269fcb8d7fd9573fead2e424f14db3ed322a8da
Taavi Päll <tapa741@gmail.com>
2019-09-05 15:16:08 +0300

    added command line arguments

--- blast/taxonomy/wrapper.py
+++ blast/taxonomy/wrapper.py
@@ -6,8 +6,10 @@ import json
 import sys
 from itertools import islice
 
-assert(sys.argv[1], "Please supply array index number!")
-chunk = sys.argv[1]
+chunk = int(sys.argv[1])
+print("We have chunk nr {}".format(chunk))
+n_chunks = int(sys.argv[2])
+print("Total number of chunks is {}".format(n_chunks))
 
 ncbi = NCBITaxa()
 
@@ -21,40 +23,41 @@ def chunks(data, n_chunks = 100):
     for i in range(0, len(data), size):
         yield {k:data[k] for k in islice(it, size)}
 
-for count, item in enumerate(chunks(by_query.indices, 3), 1):
-    if count is 3:
+taxranks = ["SUBSPECIES", "SPECIES", "GENUS", "FAMILY", "ORDER", "CLASS", "PHYLUM", "KINGDOM", "SUPERKINGDOM", "DOMAIN", "LIFE"]
+taxranks = [i.lower() for i in taxranks]
+
+def assign_consensus_taxonomy(blast_df, pp_sway, id, out_prefix):
+    consensus_taxonomy = []
+    for query, hits in by_query:
+        if hits.shape[0] > 1:
+            hits = hits.sort_values(by = "evalue")
+            pident_threshold = hits["pident"].aggregate("max") - pp_sway
+            within = hits["pident"].apply(lambda x: x >= pident_threshold)
+            hits_filtered = hits[within]
+            taxlist = hits_filtered["tax_id"].tolist()
+            lineage = []
+            for tax in taxlist:
+                lineage.append(set(ncbi.get_lineage(tax)))
+                lineage_intersect = list(set.intersection(*lineage))
+            root_tree = ncbi.get_topology(lineage_intersect)
+            consensus = root_tree.get_leaf_names()
+            assert(len(consensus) == 1)
+        else:
+            consensus = hits["tax_id"].tolist()
+        con_lin = []
+        for tax in consensus:
+            con_lin.append(ncbi.get_lineage(tax))
+        ranks = []
+        names = []
+        for tax in con_lin:
+            ranks.append(ncbi.get_rank(tax))
+            names.append(ncbi.get_taxid_translator(tax))
+        consensus_taxonomy.append({"query": query, "consensus": consensus[0], "pident": hits["pident"].aggregate("max"), "hits": hits.shape[0], "rank": json.dumps(ranks), "names": json.dumps(names)})
+        pd.DataFrame(consensus_taxonomy).to_csv("{}_{}.csv".format(out_prefix, id), index = False)
+
+for count, item in enumerate(chunks(by_query.indices, n_chunks), 1):
+    if count is chunk:
         print(count, [*item])
         print(blast.loc[[*item],])
+        assign_consensus_taxonomy(blast.loc[[*item],], pp_sway, count, "consensus_taxonomy")
 
-def assign_consensus_taxonomy(blast_dataframe):
-
-
-consensus_taxonomy = []
-for query, hits in by_query:
-    print(query)
-    if hits.shape[0] > 1:
-        hits = hits.sort_values(by = "evalue")
-        pident_threshold = hits["pident"].aggregate("max") - pp_sway
-        within = hits["pident"].apply(lambda x: x >= pident_threshold)
-        hits_filtered = hits[within]
-        taxlist = hits_filtered["tax_id"].tolist()
-        lineage = []
-        for tax in taxlist:
-            lineage.append(set(ncbi.get_lineage(tax)))
-            lineage_intersect = list(set.intersection(*lineage))
-        root_tree = ncbi.get_topology(lineage_intersect)
-        consensus = root_tree.get_leaf_names()
-        assert(len(consensus) == 1)
-    else:
-        consensus = hits["tax_id"].tolist()
-    con_lin = []
-    for tax in consensus:
-        con_lin.append(ncbi.get_lineage(tax))
-    ranks = []
-    names = []
-    for tax in con_lin:
-        ranks.append(ncbi.get_rank(tax))
-        names.append(ncbi.get_taxid_translator(tax))
-    consensus_taxonomy.append({"query": query, "consensus": consensus[0], "pident": hits["pident"].aggregate("max"), "hits": hits.shape[0], "rank": json.dumps(ranks), "names": json.dumps(names)})
-
-pd.DataFrame(consensus_taxonomy).to_csv("~/Downloads/consensus_taxonomy.csv", index = False)
