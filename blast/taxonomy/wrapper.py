39326b191164f7ca9aca465767833b57f057d050
Taavi Päll <tapa741@gmail.com>
2019-10-02 23:05:07 +0300

    updated taxonomy script, simpler, faster, added taxon names

--- blast/taxonomy/wrapper.py
+++ blast/taxonomy/wrapper.py
@@ -36,38 +36,9 @@ class BlastDB:
     def translate_to_names(self, taxids):
         return self.ncbi.translate_to_names(taxids)
 
-    def get_normalised_lineage(self, taxid, ranks_of_interest, taxonomic_ranks, unidentified):
-        # Dictionary that maps taxonomic ranks to integers,
-        # the lowest is "no rank": 0
-        numeric_rank = {taxonomic_ranks[i]: i for i in range(len(taxonomic_ranks))}
-        # Get lineage for taxid and map it to numeric rank
-        lineage = self.get_lineage(taxid)
-        lineage_ranks = self.get_rank(lineage)
-        # Keep only main taxonomic ranks 
-        lineage_ranks = {k: v for k, v in lineage_ranks.items() if v in taxonomic_ranks}
-        lineage = list(lineage_ranks.keys())
-        updated_taxid=[lineage[-1]]
-        _, rank_of_query = self.get_rank(updated_taxid).popitem() 
-        num_rank_of_query = numeric_rank[rank_of_query]
-        ranks = self.get_rank(lineage)
-        invert_dict = {v: k for k, v in ranks.items()}
-        lineage_ranks = invert_dict.keys()
-        # Get ranks and taxids
-        # if rank is not present in lineage, say is unidentified
-        normalised_lineage = dict()
-        for rank in ranks_of_interest:
-            num_rank_to_get = numeric_rank[rank]
-            # must be above taxid in hierarchy
-            if num_rank_to_get >= num_rank_of_query:
-                if rank in lineage_ranks:
-                    normalised_lineage[rank] = invert_dict[rank]
-                else:
-                    normalised_lineage[rank] = unidentified
-        return normalised_lineage
-
 class BlastTaxonomy(BlastDB):
     
-    def __init__(self, results, query_key = "qseqid", taxid_key = "staxid", pp_sway=1, ranks_of_interest=None, taxonomic_ranks=None, dbfile=None):
+    def __init__(self, results, query_key = "qseqid", taxid_key = "staxid", pp_sway=1, ranks_of_interest=None, dbfile=None):
         
         BlastDB.__init__(self, dbfile)
         self.query_key = query_key
@@ -78,10 +49,6 @@ class BlastTaxonomy(BlastDB):
             self.ranks_of_interest = ranks_of_interest
         else:
             self.ranks_of_interest = ["superkingdom", "order", "family", "genus", "species"]
-        if taxonomic_ranks:
-            self.taxonomic_ranks = taxonomic_ranks
-        else:
-            self.taxonomic_ranks = ["no rank", "species", "genus", "family", "order", "class", "phylum", "kingdom", "superkingdom"]
         self.unidentified = 32644
     
     def get_consensus_taxonomy(self):
@@ -96,6 +63,7 @@ class BlastTaxonomy(BlastDB):
                 unidentified = hits["name"].apply(lambda x: bool(re.search("unident", x)))
                 identified = np.invert(unidentified)
                 if sum(identified) >= 1:
+                    # Keeping only identified  taxids
                     hits = hits[identified]
                     # Filtering by percent identity
                     pident_threshold = hits["pident"].aggregate("max") - self.pp_sway
@@ -104,26 +72,21 @@ class BlastTaxonomy(BlastDB):
                     # Getting consensus taxonomy
                     taxlist = hits_filtered[self.taxid_key].tolist()
                     if len(taxlist) > 1:
-                        lineage = []
-                        for tax in taxlist:
-                            normalised_lineage = self.get_normalised_lineage(tax, self.ranks_of_interest, self.taxonomic_ranks, self.unidentified)
-                            rev_normalised_lineage = {v: k for k, v in normalised_lineage.items()}
-                            lineage.append(set(rev_normalised_lineage))
-                        lineage_intersect = list(set.intersection(*lineage))
-                        if len(lineage_intersect) == 0:
-                            consensus = [self.unidentified]
-                        else:
-                            root_tree = self.get_topology(lineage_intersect)
-                            consensus = root_tree.get_leaf_names()
-                            consensus = [int(txid) for txid in consensus]
+                        tree = self.get_topology(taxlist)
+                        for node in tree.traverse():
+                            if node.is_root():
+                                consensus = [node.name]
                     else:
                         consensus = taxlist
                 else:
                     consensus = [self.unidentified]
             else:
                 consensus = hits[self.taxid_key].tolist()   
-            con_lin = self.get_normalised_lineage(consensus[0], self.ranks_of_interest, self.taxonomic_ranks, self.unidentified)
-            consensus_taxonomy.append(dict({"query": query, "consensus": consensus[0], "pident": hits["pident"].aggregate("max"), "hits": hits.shape[0]}, **con_lin))
+            lineage = self.get_lineage(*consensus)
+            ranks = self.get_rank(lineage)
+            con_lin = {rank: id for id, rank in ranks.items() if rank in self.ranks_of_interest}
+            lin_names = {rank + "_name": self.translate_to_names([id])[0] for rank, id in con_lin.items()}
+            consensus_taxonomy.append(dict({"query": query, "consensus": consensus[0], "pident": hits["pident"].aggregate("max"), "hits": hits.shape[0]}, **con_lin, **lin_names))
         consensus_taxonomy = pd.DataFrame(consensus_taxonomy)
         # Convert tax_ids to integers
         consensus_taxonomy[self.ranks_of_interest] = consensus_taxonomy[self.ranks_of_interest].apply(lambda x: pd.Series(x, dtype = "Int64"))
