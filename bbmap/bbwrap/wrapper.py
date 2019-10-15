7859cdb11f107ac0682b5faf77e769773dc0bed2
Taavi Päll <tapa741@gmail.com>
2019-10-15 14:37:29 +0300

    filter output dictionary

--- bbmap/bbwrap/wrapper.py
+++ bbmap/bbwrap/wrapper.py
@@ -5,33 +5,34 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 
-# Function to concatenate arguments with names.
+# Function to concatenate arguments with names
 def arg_c(args):
    argdict = dict(args)
    arglist = ['{}={}'.format(k, ",".join(v if isinstance(v, type([])) else [v])) for k,v in argdict.items()]
    argstr = " ".join(arglist)
    return argstr
 
-# Get inputs.
+# Get inputs
 inputs = arg_c(snakemake.input)
 
 # Replace 'input' with 'in'
 inputs = inputs.replace("input", "in")
 
-# Get outputs.
-outputs = arg_c(snakemake.output)
-
 # Pass only bbmap/bbwrap output parameters
 bbmap_outputs = ["out", "outu", "outm", "scafstats", "refstats", "bhist", "qhist", "aqhist", "bqhist", "lhist", "ihist", "ehist", "qahist", "indelhist", "mhist", "gchist", "idhist", "covstats", "rpkm", "covhist", "basecov", "bincov"]
+outputs = dict(snakemake.output)
 outputs = {k:v for k, v in outputs.items() if k in bbmap_outputs}
 
-# Get extra arguments.
+# Parse outputs to argument string
+outputs = arg_c(outputs)
+
+# Get extra arguments
 extra = snakemake.params.get("extra", "")
 
-# Setup log.
+# Setup log
 log = snakemake.log_fmt_shell(stdout = False, stderr = True)
 
-# Run commands.
+# Run commands
 shell("(bbwrap.sh"
       " {inputs}"
       " {outputs}"
