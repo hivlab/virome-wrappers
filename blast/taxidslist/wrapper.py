afe05849cd35170840132de6d7f87137c340a612
Taavi Päll <tapa741@gmail.com>
2019-12-10 23:43:19 +0200

    fixed taxids list wrapper

--- blast/taxidslist/wrapper.py
+++ blast/taxidslist/wrapper.py
@@ -1,5 +1,4 @@
 import subprocess
-from os.path import dirname
 from more_itertools import always_iterable
 
 # Getting taxa for query
@@ -7,11 +6,16 @@ params = snakemake.params
 
 # Getting taxid lists
 for k, v in params.items():
-    taxlist = []
     for i in always_iterable(v):
-        cmd = "get_species_taxids.sh -t {}".format(i)
-        process = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-        taxlist.append(process.stdout)
-    with open(snakemake.output[k], "wt") as f:
-        f.write("".join(taxlist))
-
+        p = subprocess.run(
+            ["get_species_taxids.sh", "-t", str(i)],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        )
+        if "esearch error" in p.stdout:
+            raise Exception(p.stdout)
+        else:
+            with open(snakemake.output[k], "wt") as f:
+                f.write(p.stdout)
