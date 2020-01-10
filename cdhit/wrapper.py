d2209fd39b6088f05d44eed15498fdd75c64d98b
Taavi Päll <tapa741@gmail.com>
2020-01-10 11:11:21 +0200

    cdhit threads

--- cdhit/wrapper.py
+++ cdhit/wrapper.py
@@ -5,10 +5,17 @@ program = snakemake.params.get("program", "cd-hit-est")
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
+# Parse threads flag
+if snakemake.threads:
+    threads = snakemake.threads[0]
+else:
+    threads = 0
+
+
 shell(
     "({program}"
     " -i {snakemake.input}"
     " -o {snakemake.output.repres}"
-    " -T {snakemake.threads}"
+    " -T {threads}"
     " {extra}) {log}"
 )
