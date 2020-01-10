3dbeb35af3935551bb9244d1c20dc0892bea2c7c
Taavi Päll <tapa741@gmail.com>
2020-01-10 10:18:09 +0200

    assert bbwrap inputs

--- bbmap/bbwrap/wrapper.py
+++ bbmap/bbwrap/wrapper.py
@@ -7,10 +7,21 @@ from snakemake.shell import shell
 
 
 # Function to concatenate arguments with names
-def arg_c(args):
+def arg_c(args, n):
     argdict = dict(args)
+    # Convert values to list
+    argdict = {
+            k: (v if isinstance(v, type([])) else [v])
+            for k, v in argdict.items()
+        }
+    # Merge multiple argument values to comma separated str
+    argdict = {
+            k: (",".join(v + ["null"] * n_files_diff) if "in2" in k else ",".join(v))
+            for k, v in argdict.items()
+        }
+    # Parse arguments for command line
     arglist = [
-        "{}={}".format(k, ",".join(v if isinstance(v, type([])) else [v]))
+        "{}={}".format(k, v)
         for k, v in argdict.items()
     ]
     argstr = " ".join(arglist)
@@ -18,21 +29,18 @@ def arg_c(args):
 
 inputs = snakemake.input
 
+
 # Check for SE inputs and
+n_files_diff = 0
 if "in1" in inputs.keys():
     n_files = [len(v) for k, v in inputs.items() if k in ["in1", "in2"]]
     n_files_diff = n_files[0] - n_files[1]
     if n_files_diff < 0:
         raise Exception("Please check inputs: more elements in in2 than in in1.")
-    else:
-        inputs = {
-            k: (",".join(v + ["null" * n_files_diff]) if "in2" in k else ",".join(v))
-            for k, v in inputs.items()
-        }
 
 
 # Get inputs
-inputs = arg_c(inputs)
+inputs = arg_c(inputs, n_files_diff)
 
 # Replace 'input' with 'in'
 inputs = inputs.replace("input", "in")
@@ -67,7 +75,7 @@ outputs = dict(snakemake.output)
 outputs = {k: v for k, v in outputs.items() if k in bbmap_outputs}
 
 # Parse outputs to argument string
-outputs = arg_c(outputs)
+outputs = arg_c(outputs, 0)
 
 # Get extra arguments
 extra = snakemake.params.get("extra", "")
