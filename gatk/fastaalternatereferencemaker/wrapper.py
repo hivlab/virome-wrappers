245e8138fefce324c2254f570096fde336e6265c
Taavi Päll <tapa741@gmail.com>
2020-04-01 06:10:17 +0300

    added fastaalternatereferencemaker wrapper

---
+++ gatk/fastaalternatereferencemaker/wrapper.py
@@ -0,0 +1,23 @@
+__author__ = "Taavi Päll"
+__copyright__ = "Copyright 2020, Taavi Päll"
+__email__ = "taavi.pall@ut.ee"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+
+indexfeature = snakemake.params.get("indexfeature", "")
+refmaker = snakemake.params.get("refmaker", "")
+bam = snakemake.params.get("bam", "")
+if bam:
+    refmaker = " ".join(refmaker, "-I", bam)
+
+
+shell(
+        "gatk IndexFeatureFile {indexfeature} -I {snakemake.input.vcf} -O {snakemake.output.idx}"
+    )
+
+shell(
+        "gatk FastaAlternateReferenceMaker {refmaker} -V {snakemake.input.vcf} -R {snakemake.input.ref} -O {snakemake.output.fasta}"
+    )
