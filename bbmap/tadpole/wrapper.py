221cd69d75e32c2c2c287dfa0feb5ebc7bdf09b8
Taavi Päll <tapa741@gmail.com>
2020-04-13 18:47:22 +0300

    added tadpole wrapper

---
+++ bbmap/tadpole/wrapper.py
@@ -0,0 +1,56 @@
+__author__ = "Taavi Päll"
+__copyright__ = "Copyright 2020, Taavi Päll"
+__email__ = "tapa741@gmail.com"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+import re
+
+# Function to concatenate arguments with names
+def arg_c(args, n):
+    argdict = dict(args)
+    # Convert values to list
+    argdict = {k: (v if isinstance(v, type([])) else [v]) for k, v in argdict.items()}
+    # Merge multiple argument values to comma separated str
+    argdict = {
+        k: (",".join(v + ["null"] * n_files_diff) if "in2" in k else ",".join(v))
+        for k, v in argdict.items()
+    }
+    # Parse arguments for command line
+    arglist = ["{}={}".format(k, v) for k, v in argdict.items()]
+    argstr = " ".join(arglist)
+    return argstr
+
+# Parse inputs
+inputs = snakemake.input
+inputs = arg_c(inputs, 0)
+
+# Replace 'input' with 'in'
+inputs = inputs.replace("input", "in")
+
+# Parse outputs
+# Pass only reformat outputs
+reformat_outputs = [
+    "out",
+    "out2",
+    "outd",
+    "dot",
+    "dump",
+]
+outputs = dict(snakemake.output)
+outputs = {k: v for k, v in outputs.items() if k in reformat_outputs}
+
+# Parse outputs to argument string
+outputs = arg_c(outputs, 0)
+
+# Get extra arguments
+extra = snakemake.params.get("extra", "")
+
+# Setup log
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+shell(
+    """
+    (tadpole.sh {inputs} {outputs} {extra}) {log}
+    """
+)
