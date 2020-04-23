deea51c4214f413a7b079740bfd9c54daaed7816
Taavi Päll <tapa741@gmail.com>
2020-04-23 22:44:43 +0300

    added vcfcombine wrapper

---
+++ vcflib/vcfcombine/wrapper.py
@@ -0,0 +1,12 @@
+__author__ = "Taavi Päll"
+__copyright__ = "Copyright 2020, Taavi Päll"
+__email__ = "taavi.pall@ut.ee"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+
+shell(
+    "vcfcombine {snakemake.input} > {snakemake.output}"
+)
