175cdb9431fef319f94c9a6d70df3394c0d049b4
Taavi Päll <tapa741@gmail.com>
2020-06-30 15:23:15 +0300

    added bbtools/repair wrapper

---
+++ bbtools/repair/wrapper.py
@@ -0,0 +1,26 @@
+__author__ = "Taavi Päll"
+__copyright__ = "Copyright 2020, Taavi Päll"
+__email__ = "tapa741@gmail.com"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+
+
+def parseIO(d):
+    return " ".join([("in" if k == "input" else k) + "=" + v for k, v in d.items()])
+
+
+inputs = parseIO(snakemake.input)
+outputs = parseIO(snakemake.output)
+
+# Get extra arguments
+extra = snakemake.params.get("extra", "")
+
+# Setup log
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+shell(
+    """
+    (repair.sh {inputs} {outputs} {extra}) {log}
+    """
+)
