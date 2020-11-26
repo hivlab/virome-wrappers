33178535727b8944b0376110b882f9f6ce9dbb46
Taavi Päll <tapa741@gmail.com>
2020-11-26 14:33:13 +0200

    added indelqual dindel wrapper

---
+++ lofreq/indelqual/wrapper.py
@@ -0,0 +1,11 @@
+__author__ = "Taavi Päll"
+__copyright__ = "Copyright 2020, Taavi Päll"
+__email__ = "taavi.pall@ut.ee"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+shell(
+    "lofreq indelqual --dindel -f {snakemake.input.ref} -o {snakemake.output[0]} {snakemake.input.bam}"
+)
