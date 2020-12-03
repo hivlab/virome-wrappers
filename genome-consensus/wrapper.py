25fefc8fb587e2886dc6e3b601e5425721cbafa3
Taavi Päll <tapa741@gmail.com>
2020-12-03 17:04:31 +0200

    updated genome consensus wrapper

--- genome-consensus/wrapper.py
+++ genome-consensus/wrapper.py
@@ -9,6 +9,8 @@ from snakemake.shell import shell
 # Get extra arguments
 mask = snakemake.params.get("mask", 20)
 extra = snakemake.params.get("extra", "")
+reads=snakemake.input.reads
+reads=",".join([reads] if isinstance(reads, str) else reads)
 
 # Setup log
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
@@ -18,7 +20,8 @@ shell(
     (bgzip -c {snakemake.input.vcf} > {snakemake.output.vcfgz}
     bcftools index {snakemake.output.vcfgz}
     cat {snakemake.input.ref} | bcftools consensus {snakemake.output.vcfgz} > {snakemake.output.consensus}
-    bbmap.sh ref={snakemake.output.consensus} in={snakemake.input.reads} out=stdout.sam nodisk {extra} | samtools view -h -b -q 20 -f 0x3 - | genomeCoverageBed -bga -ibam stdin | awk '$4 < {mask}' | bedtools merge > {snakemake.output.bed}
+    bbwrap.sh ref={snakemake.output.consensus} in={reads} out={snakemake.output.sam} nodisk append {extra}
+    samtools view -h -b -q 20 -f 0x3 {snakemake.output.sam} | genomeCoverageBed -bga -ibam stdin | awk '$4 < {mask}' | bedtools merge > {snakemake.output.bed}
     bedtools maskfasta -fi {snakemake.output.consensus} -bed {snakemake.output.bed} -fo {snakemake.output.consensus_masked}) {log}
     """
 )
