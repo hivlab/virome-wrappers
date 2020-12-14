388bb70aa595168e4054deca2b4bf59d93151d24
Taavi Päll <tapa741@gmail.com>
2020-12-14 13:53:46 +0200

    added lofreq alnqual wrapper

---
+++ lofreq/alnqual/wrapper.py
@@ -0,0 +1,12 @@
+__author__ = "Taavi Päll"
+__copyright__ = "Copyright 2020, Taavi Päll"
+__email__ = "taavi.pall@ut.ee"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+extra = snakemake.params.get("extra", "")
+
+shell(
+    "lofreq alnqual {extra} {snakemake.input.bam} {snakemake.input.ref} > {snakemake.output[0]}"
+)
