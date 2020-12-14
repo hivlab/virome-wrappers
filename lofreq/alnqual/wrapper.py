3137a92fddc19415f894136e8dce9fb52e07346d
Taavi Päll <tapa741@gmail.com>
2020-12-14 17:45:52 +0200

    updated lofreq alnqual wrapper

--- lofreq/alnqual/wrapper.py
+++ lofreq/alnqual/wrapper.py
@@ -6,7 +6,11 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 shell(
-    "lofreq alnqual {extra} {snakemake.input.bam} {snakemake.input.ref} > {snakemake.output[0]}"
+    "lofreq alnqual {extra}"
+    " {snakemake.input.bam}"
+    " {snakemake.input.ref}"
+    " > {snakemake.output[0]} {log}"
 )
