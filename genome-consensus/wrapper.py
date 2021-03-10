6c2aa706b08eee0e694d778836f70df28af6ed2b
Taavi Päll <tapa741@gmail.com>
2021-03-10 14:30:46 +0200

    updated consensus genome wrapper

--- genome-consensus/wrapper.py
+++ genome-consensus/wrapper.py
@@ -4,24 +4,38 @@ __email__ = "tapa741@gmail.com"
 __license__ = "MIT"
 
 from snakemake.shell import shell
+import tempfile
+import os
 
 
-# Get extra arguments
-mask = snakemake.params.get("mask", 20)
-extra = snakemake.params.get("extra", "")
-reads=snakemake.input.reads
-reads=",".join([reads] if isinstance(reads, str) else reads)
+# Parameters
+mask = snakemake.params.get("mask", 5)
+mapping_quality = snakemake.params.get("mapping_quality", 0)
 
 # Setup log
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
+# Temp files
+temp_vcfgz = tempfile.TemporaryFile()
+vcfgz_index = str(temp_vcfgz.name) + ".csi"
+temp_consensus = tempfile.TemporaryFile()
+temp_bed = tempfile.TemporaryFile()
+
 shell(
     """
-    (bgzip -c {snakemake.input.vcf} > {snakemake.output.vcfgz}
-    bcftools index {snakemake.output.vcfgz}
-    cat {snakemake.input.ref} | bcftools consensus {snakemake.output.vcfgz} > {snakemake.output.consensus}
-    bbwrap.sh ref={snakemake.output.consensus} in={reads} out={snakemake.output.sam} nodisk append {extra}
-    samtools view -h -b -q 20 -f 0x3 {snakemake.output.sam} | genomeCoverageBed -bga -ibam stdin | awk '$4 < {mask}' | bedtools merge > {snakemake.output.bed}
-    bedtools maskfasta -fi {snakemake.output.consensus} -bed {snakemake.output.bed} -fo {snakemake.output.consensus_masked}) {log}
+    (bgzip -c {snakemake.input.vcf} > {temp_vcfgz.name}
+    bcftools index {temp_vcfgz.name}
+    cat {snakemake.input.ref} | bcftools consensus {temp_vcfgz.name} > {temp_consensus.name}
+    samtools view -h -b -q {mapping_quality} -f 0x3 {snakemake.input.alignment} | genomeCoverageBed -bga -ibam stdin | awk '$4 < {mask}' | bedtools merge > {temp_bed.name}
+    bedtools maskfasta -fi {temp_consensus.name} -bed {temp_bed.name} -fo {snakemake.output.consensus}) {log}
+    """
+)
+
+shell(
+    """
+    rm -f {temp_vcfgz.name}
+    rm -f {vcfgz_index}
+    rm -f {temp_consensus.name}
+    rm -f {temp_bed.name}
     """
 )
