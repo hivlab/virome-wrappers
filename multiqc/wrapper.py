27a410328df10120850980f9b909a762c753860d
Taavi Päll <tapa741@gmail.com>
2021-03-10 10:02:34 +0200

    use multiqc v1.9

--- multiqc/wrapper.py
+++ multiqc/wrapper.py
@@ -1,5 +1,3 @@
-"""Snakemake wrapper for trimming paired-end reads using cutadapt."""
-
 __author__ = "Julian de Ruiter"
 __copyright__ = "Copyright 2017, Julian de Ruiter"
 __email__ = "julianderuiter@gmail.com"
