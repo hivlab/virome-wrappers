93cd01ea76d68aee91bb5eeec069d5362d84fc38
Taavi Päll <tapa741@gmail.com>
2021-06-10 16:16:04 +0300

    added callvariants wrapper

---
+++ bbtools/callvariants/wrapper.py
@@ -0,0 +1,29 @@
+__author__ = "Taavi Päll"
+__copyright__ = "Copyright 2021, Taavi Päll"
+__email__ = "tapa741@gmail.com"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+from snakemake_wrapper_utils.java import get_java_opts
+
+
+java_opts = get_java_opts(snakemake)
+
+
+def parseIO(d):
+    return " ".join([("in" if k == "input" else k) + "=" + v for k, v in d.items()])
+
+inputs = parseIO(snakemake.input)
+outputs = parseIO(snakemake.output)
+
+# Get extra arguments
+extra = snakemake.params.get("extra", "")
+
+# Setup log
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+shell(
+    """
+    (callvariants.sh {inputs} {outputs} {extra} {java_opts}) {log}
+    """
+)
